483dbd5cd25dc972035914bbbaf3f1e49d075aa0
naychurrahl <onifadej@gmail.com>
2022-12-12 21:29:57 -0500

    commit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,9 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("{} is Positive".format(number))
-elif number < 0:
-    print("{} is Negative".format(number))
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
 else:
-    print("{} is Zero".format(number))
-
+    print(f"{number} is negative")
